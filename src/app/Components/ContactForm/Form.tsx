15bf32703429f1de5ed88c7002db5363b8d33c1d
Martin Lowe <martinlowe673@gmail.com>
2024-01-29 20:12:06 +0000

    commented out

--- src/app/Components/ContactForm/Form.tsx
+++ src/app/Components/ContactForm/Form.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import "./Form.css"
+// import "./Form.css"
 
 interface IFormInput {
   name: string;
