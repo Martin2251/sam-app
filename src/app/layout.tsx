3d54fd72d02e8a4603460378fcc10c07968c8b14
Martin Lowe <martinlowe673@gmail.com>
2024-01-29 19:44:07 +0000

    so far

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./Components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="mx-auto max-w-[2160px]">
+ 
+        {children}
+        </main>
+        
+        </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,15 @@ export default function Home() {
 <main>
   <h1>Sam fitness app</h1>
   <h2>home page</h2>
+  <main>
   <Link href="/Pages/About">
   <p>Im about</p>
   </Link>
 
-  <button className="btn btn-primary">Button</button>
+  <button className="btn btn-secondary">Button</button>
+  </main>
+ 
+
 </main>
   );
 }
