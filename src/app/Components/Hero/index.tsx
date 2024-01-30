a6b1631898586292f40aee16f881ec140d701007
Martin Lowe <martinlowe673@gmail.com>
2024-01-30 07:47:17 +0000

    added hero

---
+++ src/app/Components/Hero/index.tsx
@@ -0,0 +1,24 @@
+// components/Hero.tsx
+import React from 'react';
+import Image from 'next/image';
+
+const Hero: React.FC = () => {
+  return (
+    <div className="relative h-screen">
+      <Image
+        src="/images/cyclist.jpg" // Path to your image in the public directory
+        alt="Hero Image"
+        layout="fill"
+        objectFit="cover"
+      
+      />
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-purple-600">
+        <h1 className="text-4xl md:text-6xl font-bold mb-4">Sam Fitness App</h1>
+        <p className="text-lg md:text-xl mb-8">Your go-to place for fitness.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
+import Hero from "./Components/Hero";
 
 export default function Home() {
   return (
 <main>
-  <h1>Sam fitness app</h1>
-  <h2>home page</h2>
-  <main>
-  <Link href="/Pages/About">
-  <p>Im about</p>
-  </Link>
-
-  <button className="btn btn-secondary">Button</button>
+<Hero  />
   </main>
  
 
-</main>
+
   );
 }
