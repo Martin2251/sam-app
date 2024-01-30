4cc47297cda21b3aea8a03a1084d43ad347afae3
Martin Lowe <martinlowe673@gmail.com>
2024-01-30 07:54:19 +0000

    Train section

---
+++ src/app/Components/Train/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TrainSection = () => {
+  return (
+  <main className='bg-slate-200 '>
+    <h1 className='text-center text-black'>Train with the Best</h1>
+
+    <p className='text-center text-black'>Whether youre striving to surpass your fitness milestones or competing at an elite level to outperform your rivals, there's no superior or more enjoyable method to reach your goals than with us™. Unleash your potential and redefine your limits with our innovative approach to fitness."</p>
+  </main>
+  )
+}
+
+export default TrainSection
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import Hero from "./Components/Hero";
+import TrainSection from "./Components/Train";
 
 export default function Home() {
   return (
 <main>
 <Hero  />
+<TrainSection  />
   </main>
  
 
