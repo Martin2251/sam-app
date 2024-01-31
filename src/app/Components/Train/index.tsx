fbd057fd0f1afb812cf937c5d25181108a9e8d9a
Martin Lowe <martinlowe673@gmail.com>
2024-01-31 08:22:28 +0000

    added basic button

--- src/app/Components/Train/index.tsx
+++ src/app/Components/Train/index.tsx
@@ -6,6 +6,9 @@ const TrainSection = () => {
     <h1 className='text-center text-black'>Train with the Best</h1>
 
     <p className='text-center text-black'>Whether youre striving to surpass your fitness milestones or competing at an elite level to outperform your rivals theres no superior or more enjoyable method to reach your goals than with us™. Unleash your potential and redefine your limits with our innovative approach to fitness </p>
+    <div className='flex items-center justify-center'>
+    <button className="btn  btn-primary">Train</button>
+    </div>
   </main>
   )
 }
