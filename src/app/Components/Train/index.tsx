0974ca11620ea3e350e485df3a9f43c7f8bcf23d
Martin Lowe <martinlowe673@gmail.com>
2024-02-03 10:08:45 +0000

    margin bottom added

--- src/app/Components/Train/index.tsx
+++ src/app/Components/Train/index.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const TrainSection = () => {
   return (
-  <main className='bg-slate-200 '>
-    <h1 className='text-center text-black'>Train with the Best</h1>
+  <main className='bg-slate-200 mb-4 '>
+    <h1 className='text-center text-black mb-4'>Train with the Best</h1>
 
-    <p className='text-center text-black'>Whether youre striving to surpass your fitness milestones or competing at an elite level to outperform your rivals theres no superior or more enjoyable method to reach your goals than with us™. Unleash your potential and redefine your limits with our innovative approach to fitness </p>
-    <div className='flex items-center justify-center'>
+    <p className='text-center text-black mb-4'>Whether youre striving to surpass your fitness milestones or competing at an elite level to outperform your rivals theres no superior or more enjoyable method to reach your goals than with us™. Unleash your potential and redefine your limits with our innovative approach to fitness </p>
+    <div className='flex items-center justify-center '>
     <button className="btn  btn-primary">Train</button>
     </div>
   </main>
