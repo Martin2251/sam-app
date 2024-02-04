43fcb18e0db0b5f10988586e0a1722e7a3f53607
Martin Lowe <martinlowe673@gmail.com>
2024-02-04 19:44:46 +0000

    image getting there

--- src/app/Components/Hero/index.tsx
+++ src/app/Components/Hero/index.tsx
@@ -1,6 +1,6 @@
 // components/Hero.tsx
 import React from 'react';
-import Image from 'next/image';
+import Image from "next/legacy/image";
 
 const Hero: React.FC = () => {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import Link from "next/link";
 import Hero from "./Components/Hero";
 import TrainSection from "./Components/Train";
