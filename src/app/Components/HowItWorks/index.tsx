6fb732717f67d5dbd8a1d042933469ad99b3f99f
Martin Lowe <martinlowe673@gmail.com>
2024-02-07 07:56:44 +0000

    added scss

--- src/app/Components/HowItWorks/index.tsx
+++ src/app/Components/HowItWorks/index.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { BsSmartwatch } from 'react-icons/bs'
+import { GiArcheryTarget } from 'react-icons/gi'
+import { IoCalendarNumberOutline } from 'react-icons/io5'
+import './HowItWorks.scss';
 
 const HowItWorks = () => {
   return (
@@ -7,6 +11,14 @@ const HowItWorks = () => {
             <h1 className="text-3xl text-center">How it works</h1>
         </header>
         <p className='text-white text-center'>Optimise your training</p>
+        <section>
+        <GiArcheryTarget />
+        <p>Set your goal</p>
+        <IoCalendarNumberOutline />
+        <p>Set Weekly avaliabity</p>
+        <BsSmartwatch />
+        <p className='wearable'>Connect your wearable device</p>
+        </section>
 
     </main>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
 <TrainSection  />
 <HowItWorks  />
 <StayAhead  />
+<HowItWorks />
   </main>
  
 
