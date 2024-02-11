a8648955db762eee640bdb9f86c7a5b089161fbe
Martin Lowe <martinlowe673@gmail.com>
2024-02-11 17:57:49 +0000

    adding media query

--- src/app/Components/HowItWorks/index.tsx
+++ src/app/Components/HowItWorks/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BsSmartwatch } from 'react-icons/bs'
+import { BsCardChecklist, BsSmartwatch } from 'react-icons/bs'
 import { GiArcheryTarget } from 'react-icons/gi'
 import { IoCalendarNumberOutline } from 'react-icons/io5'
 import './HowItWorks.scss';
@@ -11,14 +11,18 @@ const HowItWorks = () => {
             <h1 className="text-3xl text-center">How it works</h1>
         </header>
         <p className='text-white text-center'>Optimise your training</p>
-        <section>
+        <section className='how-it-works'>
+            <div className='icon-div'>
         <GiArcheryTarget />
         <p>Set your goal</p>
+        </div>
         <IoCalendarNumberOutline />
         <p>Set Weekly avaliabity</p>
         <BsSmartwatch />
         <p className='wearable'>Connect your wearable device</p>
-        
+        <BsCardChecklist />
+        <p>Replan</p>
+
         </section>
 
     </main>
