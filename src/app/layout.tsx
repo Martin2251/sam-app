b8ab3b2d198d2a583211967c93106e6d85672878
Martin Lowe <martinlowe673@gmail.com>
2024-02-12 20:59:51 +0000

    mobile ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       
       <body className={inter.className}>
-        <main className="mx-auto max-w-[2160px]">
+        <main className="">
  
         {children}
         <Footer  />
