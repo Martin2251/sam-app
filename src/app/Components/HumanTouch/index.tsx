fa90d48f178e7859faf3043c76ccd5e72656bbbd
Martin Lowe <martinlowe673@gmail.com>
2024-02-12 20:56:59 +0000

    small background change

--- src/app/Components/HumanTouch/index.tsx
+++ src/app/Components/HumanTouch/index.tsx
@@ -4,7 +4,7 @@ import "./HumanTouch.scss"
 const HumanTouch = () => {
 
   return (
-    <main className='container'>
+    <main className='container bg-amber-400'>
         <div className='content'>
         <header>
       <h3>Human Touch</h3>
