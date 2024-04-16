1232878bb5163108ba48842cdcfbfc37e9c8a555
Martin Lowe <martinlowe673@gmail.com>
2024-04-16 07:40:31 +0100

    changing footer

--- src/app/Components/Footer/index.tsx
+++ src/app/Components/Footer/index.tsx
@@ -1,10 +1,16 @@
+// Footer.tsx
 import React from 'react';
+import './Footer.scss';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-900 text-white py-4">
+    <footer className="footer">
       <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} Sam Fitness App</p>
+        <ul className="footer__list">
+          <li className="footer__item">hi</li>
+          <li className="footer__item">hi</li>
+        </ul>
+        <p className="footer__copyright">&copy; {new Date().getFullYear()} Sam Fitness App</p>
       </div>
     </footer>
   );
