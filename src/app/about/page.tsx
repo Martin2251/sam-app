0ed2355e7a414885e178c85edad7c39b760c4c7a
Martin Lowe <martinlowe673@gmail.com>
2024-04-17 20:14:33 +0100

    amended about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,12 +5,12 @@ import './About.scss';
 const AboutPage = () => {
   return (
     <>
-    <header>
+    <header className='heading'>
       <h1>Abou !!!!t</h1>
     </header>
     <main>
     <section>
-      <div>
+      <div className='about-text'>
       <p>Balancing science and emotion runs deep in our veins. Our founder, Dr. Eric Abecassis has always personified – in his own ways – the beauty that sits at the intersection of technology and passion.</p>
       <p>Eric has achieved the kind of sport-life balance that many of us only dream of. Luckily, he’s brought together a valuable team of like-minded sports and life enthusiasts, experts in training, AI, machine learning, recovery, and more, and we’ve now empowered an app to help everyone do the same – regardless if you’re at the beginning, middle, or peak of your training journey.</p>
       </div>
