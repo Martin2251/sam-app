8f92ca8aee361f063db1a88969a4f80042a065bb
Martin Lowe <martinlowe673@gmail.com>
2024-04-22 19:02:20 +0100

    coaches map

--- src/app/Components/AboutPage/AboutComponents/Coaches/Coaches.tsx/Coaches.tsx
+++ src/app/Components/AboutPage/AboutComponents/Coaches/Coaches.tsx/Coaches.tsx
@@ -1,26 +1,69 @@
-import React from 'react'
-import { Row } from 'react-bootstrap'
+import React from 'react';
+import { Row } from 'react-bootstrap';
+import './Coaches.scss';
 
 const Coaches = () => {
+  // Array of coach objects
+  const coaches = [
+    {
+      id: 1,
+      name: 'Dr man',
+      position: 'Founder & CEO',
+      imgSrc: 'https://kitt.lewagon.com/placeholder/users/cveneziani'
+    },
+    {
+        id: 1,
+        name: 'Dr man',
+        position: 'Founder & CEO',
+        imgSrc: 'https://kitt.lewagon.com/placeholder/users/cveneziani'
+      },
+      {
+        id: 1,
+        name: 'Dr man',
+        position: 'Founder & CEO',
+        imgSrc: 'https://kitt.lewagon.com/placeholder/users/cveneziani'
+      },
+      {
+        id: 1,
+        name: 'Dr man',
+        position: 'Founder & CEO',
+        imgSrc: 'https://kitt.lewagon.com/placeholder/users/cveneziani'
+      },
+      {
+        id: 1,
+        name: 'Dr man',
+        position: 'Founder & CEO',
+        imgSrc: 'https://kitt.lewagon.com/placeholder/users/cveneziani'
+      },
+      {
+        id: 1,
+        name: 'Dr man',
+        position: 'Founder & CEO',
+        imgSrc: 'https://kitt.lewagon.com/placeholder/users/cveneziani'
+      },
+    // Add more coaches as needed
+  ];
+
   return (
     <main className='team-member-collection'>
-        <Row>
-        <div className="col-12">
-            <div className='team-member-box'>
-                <div className='team-member-outbox'>
-                    <div className="team-member-img">
-                        <img  alt="avatar" src="https://kitt.lewagon.com/placeholder/users/cveneziani" />
-                    </div>
-                    <h3>Dr man</h3>
-                    <p>Founder & CEO</p>
+      <Row>
+        <div className="col-8">
+          {/* Map over coaches array */}
+          {coaches.map(coach => (
+            <div key={coach.id} className='team-member-box'>
+              <div className='team-member-outbox'>
+                <div className="team-member-img">
+                  <img alt="avatar" src={coach.imgSrc} />
                 </div>
-
+                <h3>{coach.name}</h3>
+                <p>{coach.position}</p>
+              </div>
             </div>
-            </div>
-        </Row>
-
+          ))}
+        </div>
+      </Row>
     </main>
-  )
+  );
 }
 
-export default Coaches
+export default Coaches;
