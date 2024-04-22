670b1e62882cf19d7cd9dfc4172a1a2b881c3876
Martin Lowe <martinlowe673@gmail.com>
2024-04-22 08:04:30 +0100

    start of coaches

--- src/app/Components/AboutPage/AboutComponents/Coaches/Coaches.tsx/Coaches.tsx
+++ src/app/Components/AboutPage/AboutComponents/Coaches/Coaches.tsx/Coaches.tsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Coaches = () => {
   return (
     <main>
-        
+        <div className="row">
+        <div className="col-12">
+            <div className='team-member-box'>
+
+            </div>
+            </div>
+        </div>
+
     </main>
   )
 }
