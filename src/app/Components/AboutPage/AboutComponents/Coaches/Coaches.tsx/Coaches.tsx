b685ee420908001cc0422437dc539838e4db3964
Martin Lowe <martinlowe673@gmail.com>
2024-04-22 18:35:48 +0100

    coaches image

--- src/app/Components/AboutPage/AboutComponents/Coaches/Coaches.tsx/Coaches.tsx
+++ src/app/Components/AboutPage/AboutComponents/Coaches/Coaches.tsx/Coaches.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import { Row } from 'react-bootstrap'
 
 const Coaches = () => {
   return (
-    <main>
-        <div className="row">
+    <main className='team-member-collection'>
+        <Row>
         <div className="col-12">
             <div className='team-member-box'>
+                <div className='team-member-outbox'>
+                    <div className="team-member-img">
+
+                    </div>
+                    <h3>Dr man</h3>
+                    <p>Founder & CEO</p>
+                </div>
 
             </div>
             </div>
-        </div>
+        </Row>
 
     </main>
   )
