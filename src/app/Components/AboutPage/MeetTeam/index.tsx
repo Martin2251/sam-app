2020b730c856deeee3a49b9c87e4d4a7ba2c343e
Martin Lowe <martinlowe673@gmail.com>
2024-04-24 19:43:03 +0100

    changing components

--- src/app/Components/AboutPage/MeetTeam/index.tsx
+++ src/app/Components/AboutPage/MeetTeam/index.tsx
@@ -5,7 +5,7 @@ const MeetTeam = () => {
    <main>
     <header>
         <h1>Meet</h1>
-        <h2>Our Team</h2>
+        <h3>Our Team</h3>
         <section>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non urna nec nisl tempus convallis ut sed turpis.</p>
             <p>aload of text</p>
--- src/app/Components/Hero/index.tsx
+++ src/app/Components/Hero/index.tsx
@@ -13,7 +13,7 @@ const Hero: React.FC = () => {
       
       />
       <div className="absolute inset-0 flex flex-col items-center justify-center text-purple-600">
-        <h1 className="text-4xl md:text-6xl font-bold mb-4">Sam Fitness App</h1>
+        <h1 >Sam Fitness App</h1>
         <p className="text-lg md:text-xl mb-8">Your go-to place for fitness.</p>
       </div>
     </div>
