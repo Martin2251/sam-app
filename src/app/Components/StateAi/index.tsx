3b996b1c9b085e40bd4958925ab8830e2b926f9f
Martin Lowe <martinlowe673@gmail.com>
2024-04-25 07:35:22 +0100

    bootstrap on state of ai

---
+++ src/app/Components/StateAi/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import" ./StateAi.scss"
+import { Row } from 'react-bootstrap'
+
+const StateOfArtAi = () => {
+  return (
+    <main>
+        <h3>State of Art Ai</h3>
+        <Row>
+        
+
+        </Row>
+    </main>
+  )
+}
+
+export default StateOfArtAi
