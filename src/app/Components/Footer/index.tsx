4df6b6cdec1cf836905075d4480fb1d79a9a9813
Martin Lowe <martinlowe673@gmail.com>
2024-04-28 20:03:54 +0100

    links and footer file

--- src/app/Components/Footer/index.tsx
+++ src/app/Components/Footer/index.tsx
@@ -2,11 +2,22 @@
 import React from 'react';
 import Link from 'next/link';
 import './Footer.scss';
+import { Row } from 'react-bootstrap';
 
 const Footer: React.FC = () => {
   return (
  
       <main className="container">
+        <Row>
+          <div className="content">
+            <ul className="footerNav">
+              <li>About</li>
+              <li>Contact</li>
+              <li>Privacy</li>
+            </ul>
+          </div>
+
+        </Row>
       
         <p className="footer__copyright">&copy; {new Date().getFullYear()} Sam Fitness App</p>
    
