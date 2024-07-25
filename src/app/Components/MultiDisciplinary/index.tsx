d26dba0e66491244f11edd7f838e7475bf76bfaf
Martin Lowe <martinlowe673@gmail.com>
2024-07-25 07:56:53 +0100

    multidiscpinary

---
+++ src/app/Components/MultiDisciplinary/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+const MultiDisciplinary = () => {
+  return (
+<Container>
+    <Row>
+    <Col md={12} lg={6}>
+    </Col>
+    </Row>
+</Container>
+  )
+}
+
+export default MultiDisciplinary
