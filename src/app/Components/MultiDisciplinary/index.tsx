9e528bb022483c017d389aeda20381d035490315
Martin Lowe <martinlowe673@gmail.com>
2024-07-25 07:59:43 +0100

    button and multi taken shape

--- src/app/Components/MultiDisciplinary/index.tsx
+++ src/app/Components/MultiDisciplinary/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 
 const MultiDisciplinary = () => {
   return (
 <Container>
     <Row>
     <Col md={12} lg={6}>
+    <div className='multidisciplinary'>
+        <h3>MultiDiscpinary</h3>
+        <small>team</small>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, ullam!</p>
+        <Button>Meet the Team</Button>
+    </div>
     </Col>
     </Row>
 </Container>
